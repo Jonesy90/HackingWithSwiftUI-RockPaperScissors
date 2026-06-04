3c78bac9c43d139ca1dd43d3625b4b4e06893af6
Michael Jones <michael.jones90@me.com>
2026-06-04 09:45:44 +0100

    Initial Commit

---
+++ HackingWithSwiftUI-RockPaperScissors/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HackingWithSwiftUI-RockPaperScissors
+//
+//  Created by Michael Jones on 04/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HackingWithSwiftUI-RockPaperScissors/HackingWithSwiftUI_RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackingWithSwiftUI_RockPaperScissorsApp.swift
+//  HackingWithSwiftUI-RockPaperScissors
+//
+//  Created by Michael Jones on 04/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct HackingWithSwiftUI_RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
